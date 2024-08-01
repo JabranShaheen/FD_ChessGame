01852f336d4a5319ca225dc54454f59b86ef47cd
JabranShaheen <JabranShaheen@hotmail.com>
2024-08-01 21:12:17 +0100

    - Added blank implementation for MinePlacer class - Created unit tests for MinePlacer - Unit tests include verifying the correct number of mines placed and ensuring no overlapping mines - Tests currently failing due to unimplemented MinePlacer methods

---
+++ FD_ChessGame/FD_ChessGame,Tests/MinePlacerTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using FD_ChessGame.Abstractions;
+using FD_ChessGame.Implementations;
+
+namespace FD_ChessGame.Tests
+{
+    [TestClass]
+    public class MinePlacerTests
+    {
+        [TestMethod]
+        public void PlaceMines_PlacesCorrectNumberOfMines()
+        {
+            // Arrange
+            int mineCount = 5;
+            var boardMock = new Mock<IBoard>();
+            boardMock.Setup(b => b.IsWithinBounds(It.IsAny<int>(), It.IsAny<int>())).Returns(true);
+            boardMock.Setup(b => b.IsMine(It.IsAny<int>(), It.IsAny<int>())).Returns(false);
+            var minePlacer = new MinePlacer();
+
+            // Act
+            minePlacer.PlaceMines(boardMock.Object, mineCount);
+
+            // Assert
+            // Verify that the PlaceMines method is called the number of times specified by mineCount
+            // Here we are verifying that the method call to PlaceMines is happening the expected number of times
+            boardMock.Verify(b => b.PlaceMines(It.IsAny<int>()), Times.Exactly(mineCount));
+        }
+
+        [TestMethod]
+        public void PlaceMines_DoesNotOverlapMines()
+        {
+            // Arrange
+            int mineCount = 10;
+            var boardMock = new Mock<IBoard>();
+            var random = new Random();
+            var placedMines = new HashSet<(int, int)>();
+            boardMock.Setup(b => b.IsWithinBounds(It.IsAny<int>(), It.IsAny<int>())).Returns(true);
+            boardMock.Setup(b => b.IsMine(It.IsAny<int>(), It.IsAny<int>())).Returns((int row, int column) => !placedMines.Add((row, column)));
+
+            var minePlacer = new MinePlacer();
+
+            // Act
+            minePlacer.PlaceMines(boardMock.Object, mineCount);
+
+            // Assert
+            Assert.AreEqual(mineCount, placedMines.Count);
+        }
+    }
+}
---
+++ FD_ChessGame/FD_ChessGame.Implementations/MinePlacer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using FD_ChessGame.Abstractions;
+
+namespace FD_ChessGame.Implementations
+{
+    public class MinePlacer : IMinePlacer
+    {
+        public void PlaceMines(IBoard board, int mineCount)
+        {
+            // Placeholder for mine placement logic
+            // Example logic might include randomly placing `mineCount` landmines on the board.
+        }
+    }
+}
