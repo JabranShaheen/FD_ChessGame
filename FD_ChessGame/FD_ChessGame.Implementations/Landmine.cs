18a22d9dd9b7b942ce98496fe54ceb5258607a3a
JabranShaheen <JabranShaheen@hotmail.com>
2024-08-01 20:56:41 +0100

    - Implemented Landmine class with position handling and position check functionality

--- FD_ChessGame/FD_ChessGame.Implementations/Landmine.cs
+++ FD_ChessGame/FD_ChessGame.Implementations/Landmine.cs
@@ -4,20 +4,16 @@ namespace FD_ChessGame.Implementations
 {
     public class Landmine
     {
-        // Position of the landmine on the board
         public Position Position { get; private set; }
 
-        // Constructor to initialize the position of the landmine
         public Landmine(Position position)
         {
             Position = position;
         }
 
-        // Method to determine if the landmine is at a specific position
         public bool IsAtPosition(Position position)
         {
-            // Implementation will be added later
-            return false;
+            return Position.Row == position.Row && Position.Column == position.Column;
         }
     }
 }
