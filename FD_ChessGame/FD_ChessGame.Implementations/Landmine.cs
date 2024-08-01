fb02c2a70cf6bd20618714165c83e16a5908a71a
JabranShaheen <JabranShaheen@hotmail.com>
2024-08-01 20:55:40 +0100

    - Added blank implementation for Landmine class - Created initial unit tests for Landmine class; tests currently failing due to incomplete implementation

---
+++ FD_ChessGame/FD_ChessGame.Implementations/Landmine.cs
@@ -0,0 +1,23 @@
+﻿using FD_ChessGame.Abstractions;
+
+namespace FD_ChessGame.Implementations
+{
+    public class Landmine
+    {
+        // Position of the landmine on the board
+        public Position Position { get; private set; }
+
+        // Constructor to initialize the position of the landmine
+        public Landmine(Position position)
+        {
+            Position = position;
+        }
+
+        // Method to determine if the landmine is at a specific position
+        public bool IsAtPosition(Position position)
+        {
+            // Implementation will be added later
+            return false;
+        }
+    }
+}
